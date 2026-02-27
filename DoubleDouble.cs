3aa950b7fa4f52b92ef6a6bf624eb9f8d5ed86f7
pcmccart17 <a00320460@mycambrian.ca>
2026-02-27 00:41:43 -0500

    I have added my variables and my argumented constructor

--- DoubleDouble.cs
+++ DoubleDouble.cs
@@ -8,6 +8,25 @@ namespace BRAIN_OBJECTS_MADE_REAL_ACTUAL_PatrickM
 {
     internal class DoubleDouble
     {
+        //My variables
+
+        private int usesLeftover;
+        private float speedBoost;
+        private bool isActive;
+        private float boostTime;
+
+        private float originalBoostTime;
+
+        //Argumented Constructor
+
+        public DoubleDouble(int usesLeftover, float speedBoost, float boostTime)
+        {
+            this.usesLeftover = usesLeftover;
+            this.speedBoost = speedBoost;
+            this.boostTime = boostTime;
+            this.originalBoostTime = boostTime;
+            this.isActive = false;
+        }
 
     }
 }
