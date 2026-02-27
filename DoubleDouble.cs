7a34a4176a54d0829e071ce9a077d23d3ca63116
pcmccart17 <a00320460@mycambrian.ca>
2026-02-27 00:37:47 -0500

    Add project files.

---
+++ DoubleDouble.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BRAIN_OBJECTS_MADE_REAL_ACTUAL_PatrickM
+{
+    internal class DoubleDouble
+    {
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new BRAIN_OBJECTS_MADE_REAL_ACTUAL_PatrickM.Game1();
+game.Run();
